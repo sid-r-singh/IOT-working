de3fe7ace67a1d478ae84fec2e606c66ed70d9be
sid-r-singh <ofw3210@gmail.com>
2020-09-13 23:50:31 +0530

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# js-c5fqnx
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/js-c5fqnx)
---
+++ index.js
@@ -0,0 +1,26 @@
+// Import stylesheets
+import './style.css';
+
+// Write Javascript code!
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+var firebaseConfig = {
+  apiKey: "AIzaSyBFhmFjnzZslcHs7rJFKoHJQrgzfTVYVeY",
+  authDomain: "mt-iot-brn.firebaseapp.com",
+  databaseURL: "https://mt-iot-brn.firebaseio.com",
+  projectId: "mt-iot-brn",
+  storageBucket: "mt-iot-brn.appspot.com",
+  messagingSenderId: "414738283359",
+  appId: "1:414738283359:web:6c7f71b4c0db262e9dd4ea",
+  measurementId: "G-36H6XGSRE4"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+var database = firebase.database();
+var ref = database.ref("Tanks/tank1");
+ref.on("value", (snapshot) => {
+document.getElementById("pnt1").innerHTML = snapshot.val();
+});
+
+
